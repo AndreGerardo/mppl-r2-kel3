f5ecfc8be57629a013336b614bd04aff885d6e37
AndreGerardo <andregerardo21@gmail.com>
2021-11-02 19:37:20 +0700

    Initial Push

---
+++ workpad/pages/auth.js
@@ -0,0 +1,13 @@
+import React from "react";
+import firebase from "../firebase/firebaseInit";
+
+function SignInScreen(){
+    return(
+        <div>
+            <h1>Company Workpad</h1>
+            <p>Sign in :</p>
+        </div>  
+    );
+}
+
+export default SignInScreen;
